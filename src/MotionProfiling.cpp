ff41c7eb01861dc14e829203a9150f837e7f3a9a
Matt Wildman <shardstealth@gmail.com>
2018-01-15 19:56:15 -0600

    Just working on it 1/15/18

--- src/MotionProfiling.cpp
+++ src/MotionProfiling.cpp
@@ -7,12 +7,35 @@
 
 #include <MotionProfiling.h>
 
-MotionProfiling::MotionProfiling()
+MotionProfiling::MotionProfiling(WPI_TalonSRX *rightFront, WPI_TalonSRX *rightBack, WPI_TalonSRX *leftFront, WPI_TalonSRX *leftBack)
 {
+	rightFrontTalon = rightFront;
+	rightBackTalon = rightBack;
+	leftFrontTalon = leftFront;
+	leftBackTalon = leftBack;
 
+	rightBackTalon->Follow(*rightFrontTalon);
+	leftBackTalon->Follow(*leftFrontTalon);
+
+	rightFrontTalon->ClearMotionProfileTrajectories();
+	leftFrontTalon->ClearMotionProfileTrajectories();
+
+	rightFrontTalon->ChangeMotionControlFramePeriod(5);
+	leftFrontTalon->ChangeMotionControlFramePeriod(5);
+
+	m_notifier = &MotionProfiling::PeriodicFeed();
+	m_notifier.StartPeriodic(0.005);
+}
+
+void MotionProfiling::PeriodicFeed()
+{
 
 }
 
+void MotionProfiling::Init()
+{
+
+}
 
 MotionProfiling::~MotionProfiling()
 {
--- src/MotionProfiling.h
+++ src/MotionProfiling.h
@@ -2,16 +2,32 @@
  * MotionProfiling.h
  *
  *  Created on: Jan 13, 2018
- *      Author: Developer
+ *      Author: Matt Wildman
  */
 
 #ifndef SRC_MOTIONPROFILING_H_
 #define SRC_MOTIONPROFILING_H_
+#include <ctre\Phoenix.h>
+#include <Notifier.h>
 
 class MotionProfiling {
 public:
-	MotionProfiling();
+	MotionProfiling(WPI_TalonSRX *rightFront, WPI_TalonSRX *rightBack, WPI_TalonSRX *leftFront, WPI_TalonSRX *leftBack);
+	void Init();
 	virtual ~MotionProfiling();
+
+protected:
+
+	void PeriodicFeed();
+	//For feeding points into the buffer
+	Notifier m_notifier;
+
+
+
+	WPI_TalonSRX *rightFrontTalon;
+	WPI_TalonSRX *rightBackTalon;
+	WPI_TalonSRX *leftFrontTalon;
+	WPI_TalonSRX *leftBackTalon;
 };
 
 #endif /* SRC_MOTIONPROFILING_H_ */
