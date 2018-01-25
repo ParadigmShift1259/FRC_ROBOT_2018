976e1cbbca81b3521a72267e43710cdf2ed9efde
Matt <shardstealth@gmail.com>
2018-01-24 19:06:44 -0600

    Found why code wouldn't start on bot, now just needs uncommenting

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -23,12 +23,9 @@ void Robot::RobotInit()
 
 	m_driverstation = &DriverStation::GetInstance();
 
-	m_motionmagic = new MotionMagic(m_drivetrain->RightTalonLead(), m_drivetrain->RightTalonFollow(),
-			m_drivetrain->LeftTalonLead(), m_drivetrain->LeftTalonFollow(), m_operatorinputs);
-
-	m_drivetrain = new DriveTrain(DriveTrain::DriveMode::kTank, m_operatorinputs);
-
 	m_drivetrain = new DriveTrain(DriveTrain::DriveMode::kCurvature, m_operatorinputs);
+
+	m_motionmagic = new MotionMagic(m_drivetrain->RightTalonLead(), m_drivetrain->RightTalonFollow(), m_drivetrain->LeftTalonLead(), m_drivetrain->LeftTalonFollow(), m_operatorinputs);
 
 	m_compressor = new Compressor(PCM_COMPRESSOR_SOLENOID);
 	m_lifter = new Lifter(m_operatorinputs);
@@ -39,64 +36,72 @@ void Robot::RobotInit()
 
 void Robot::AutonomousInit()
 {
-	m_motionmagic->Init();
-	m_motionmagic->testDriveInit();
+//	m_motionmagic->Init();
+//	m_motionmagic->testDriveInit();
 }
 
 
 void Robot::AutonomousPeriodic()
 {
-	m_motionmagic->testDrive();
+//	m_motionmagic->testDrive();
 	//m_motionmagic->Loop(10000);//Arbitrary
 }
 
 
 void Robot::TestInit()
 {
+/*
 	DriverStation::ReportError("TestInit");
 	m_compressor->Start();
 	m_drivetrain->Init();
 	m_lifter->Init();
 	m_grabber->Init();
 	m_climber->Init();
+*/
 }
 
 
 void Robot::TestPeriodic()
 {
+/*
 	m_drivetrain->Loop();
 	m_lifter->Loop();
 	m_grabber->Loop();
 	m_climber->Loop();
+*/
 }
 
 
 void Robot::TeleopInit()
 {
 	DriverStation::ReportError("TeleopInit");
+/*
 	m_compressor->Start();
 	m_drivetrain->Init();
 	m_lifter->Init();
 	m_grabber->Init();
 	m_climber->Init();
+*/
 }
 
 
 void Robot::TeleopPeriodic()
 {
+/*
 	m_drivetrain->Loop();
 	m_lifter->Loop();
 	m_grabber->Loop();
 	m_climber->Loop();
+*/
 }
 
 
 void Robot::DisabledInit()
 {
-	m_drivetrain->Stop();
+	/*m_drivetrain->Stop();
 	m_lifter->Stop();
 	m_grabber->Stop();
-	m_climber->Stop();
+	m_climber->Stop();*/
 }
 
 
